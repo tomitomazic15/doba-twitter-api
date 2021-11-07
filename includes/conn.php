10ab40b548cf49347b1515b4e0da058df3d24b33
tomitomazic15 <tomitomazic15@gmail.com>
2021-11-07 01:10:58 +0100

    search added

---
+++ includes/conn.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('TwitterAPIExchange.php');
+require_once('index.php');
+
+$settings = array(
+    'oauth_access_token' => "1370846418349518849-LQjgh6J8Mn8F8dnDZWNgRdwF83cCD5",
+    'oauth_access_token_secret' => "BUZCtLgfJEJHddA2JqEDJQgMVz80ZES6Odwe4vOyFq6M2",
+    'consumer_key' => "PhOqHQlCbJD0h6ua5I5czv3lb",
+    'consumer_secret' => "KzEU4fihMfdQBnvrR6MJDjBMJtwmwPSJPAzSo5xEonhYY8NRIT"
+);
+
+//call
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+ 
+$requestMethod = "GET";
+
+$twitter_user = isset($_GET['input']);
+
+if (preg_match('/[A-Za-z]/', $twitter_user) || preg_match('/[0-9]/', $twitter_user)){
+    $twitter_user = $_GET["input"];
+}
+
+else {
+    $twitter_user = "Twitter";
+}
+
+//twitter user's tweets
+$getfield = "?screen_name=$twitter_user&count=6&tweet_mode=extended";
+ 
+$twitter = new TwitterAPIExchange($settings);
+
+$string = json_decode($twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest(),$assoc = TRUE);
+             
+         
+            //  print_r($string)
+
+?>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php include 'includes/conn.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>TwitterAPI</title>
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand">TwitterAPI</a>
+
+  <form class="form-inline my-2 my-lg-0" method="get">
+      <input name="input" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
+<br>
+
+
+<?php foreach($string as $items) : ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <h4><?php echo $items['user']['name'];?></h4>
+                <h6 class="text-muted">@<?php echo $items['user']['screen_name'];?></h6>
+            </div>
+            <div class="col-2">
+                <?php  echo substr($items['created_at'], 4, 12);?>
+            </div>
+        </div>
+
+        <div class="card border border-primary">
+            <div class="card-body">
+                <?php echo $items['full_text'];?>
+            </div>
+        </div>
+        <div class="row pt-2">
+            <div class="col-2 green-bg">
+                <?php echo "<p>Likes: ".$items['favorite_count']."</p>";?>
+            </div>
+            <div class="col-2 green-bg">
+                <?php echo "<p>Retweets: ".$items['retweet_count']."</p>";?>
+            </div>
+        </div>
+    </div>
+    <br>
+  <?php endforeach; ?>
+</body>
+</html>
